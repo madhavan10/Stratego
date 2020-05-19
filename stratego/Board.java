da0f7093ea43169ba2224dd7423bd7a0784aa1a6
Madhavan Somanathan <madhavansomanathan@gmail.com>
2020-05-19 14:09:59 +0530

    refactor

--- stratego/Board.java
+++ stratego/Board.java
@@ -1066,6 +1066,33 @@ public class Board extends JPanel {
 			spPanel.revalidate();
 		}
 		
+		private class SPSM {
+			boolean usingSpecialPower = false;
+			boolean flight = false;
+			boolean detectEnemy = false;
+			boolean longbow = false;
+			Piece repeatAttacker = null;
+			boolean swiftSteed = false;
+			Square dwarvenAxeInitial = null;
+			Square dwarvenAxeMove = null;
+			Square[] dwarvenAxeTargets = new Square[3];
+			int dwarvenAxeTargetNumber = 0;
+			Square rampageInitial = null;
+			Square rampageMove = null;
+
+			public void setAllFalse() {
+				usingSpecialPower = false;
+				flight = detectEnemy = longbow = swiftSteed = false;
+				dwarvenAxeInitial = dwarvenAxeMove = null;
+				for(int i = 0; i < dwarvenAxeTargets.length; i++)
+					dwarvenAxeTargets[i] = null;
+				dwarvenAxeTargetNumber = 0;
+				rampageInitial = rampageMove = null;
+				repeatAttacker = null;
+			}
+
+		}
+		
 		private class SquareMotionListener extends MouseMotionAdapter {
 			@Override
 			public void mouseDragged(MouseEvent e) {
